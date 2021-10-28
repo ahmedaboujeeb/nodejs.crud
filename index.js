677ea7a2c1a1a797bcf229fd7f3dfe1259d9268c
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-10-28 11:24:14 +0200

    added create member functionality, uuid

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
-const members = require('./Members');
 
 const app = express();
 
-
 // Init middleware
-app.use(logger);
+//app.use(logger);
 
-// Gets all members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-})
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
